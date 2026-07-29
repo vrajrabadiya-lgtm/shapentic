09fd84f0c867c6b04cbdf31591a96d8a044e6954
vraj rabadiya <rabadiyavraj0@gmail.com>
2026-07-29 19:32:47 +0530

    env

--- server/index.js
+++ server/index.js
@@ -20,6 +20,8 @@ const allowedOrigins = [
   "http://localhost:5174",
   "http://127.0.0.1:5174",
   "http://localhost:3000",
+  "https://www.shapentic.com",
+  "https://shapentic.com",
   // CLIENT_ORIGIN = your Vercel deployment URL (set in Render env vars)
   process.env.CLIENT_ORIGIN,
   process.env.CLIENT_URL,
